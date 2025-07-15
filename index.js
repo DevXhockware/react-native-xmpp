322314d6a470dd58c4075eddd7b6d7526708bf31
DevXhockware <dev@xhockware.com>
2025-07-15 17:09:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,152 +1,168 @@
 'use strict';
-var React = require('react-native');
-var {NativeAppEventEmitter, NativeModules} = React;
-var RNXMPP = NativeModules.RNXMPP;
 
-var map = {
-    'message' : 'RNXMPPMessage',
-    'iq': 'RNXMPPIQ',
-    'presence': 'RNXMPPPresence',
-    'connect': 'RNXMPPConnect',
-    'disconnect': 'RNXMPPDisconnect',
-    'error': 'RNXMPPError',
-    'loginError': 'RNXMPPLoginError',
-    'login': 'RNXMPPLogin',
-    'roster': 'RNXMPPRoster'
+import { Platform, NativeModules, NativeEventEmitter } from 'react-native';
+
+if (Platform.OS === 'web') {
+  console.warn('react-native-xmpp is not supported on web');
+  const stub = {
+    connect: () => console.warn('XMPP connect not supported on web'),
+    message: () => {},
+    disconnect: () => {},
+    on: () => {},
+    removeListener: () => {},
+    removeListeners: () => {},
+    trustHosts: () => {},
+    sendStanza: () => {},
+    fetchRoster: () => {},
+    presence: () => {},
+    removeFromRoster: () => {},
+    disconnectAfterSending: () => {},
+  };
+  module.exports = stub;
+  return;
 }
 
-const LOG = (message) => {
+const RNXMPP = NativeModules.RNXMPP;
+const emitter = new NativeEventEmitter(RNXMPP);
+
+const map = {
+  message: 'RNXMPPMessage',
+  iq: 'RNXMPPIQ',
+  presence: 'RNXMPPPresence',
+  connect: 'RNXMPPConnect',
+  disconnect: 'RNXMPPDisconnect',
+  error: 'RNXMPPError',
+  loginError: 'RNXMPPLoginError',
+  login: 'RNXMPPLogin',
+  roster: 'RNXMPPRoster',
+};
+
+const LOG = message => {
   if (__DEV__) {
     console.log('react-native-xmpp: ' + message);
   }
-}
+};
 
 class XMPP {
-    PLAIN = RNXMPP.PLAIN;
-    SCRAM = RNXMPP.SCRAMSHA1;
-    MD5 = RNXMPP.DigestMD5;
+  PLAIN = RNXMPP.PLAIN;
+  SCRAM = RNXMPP.SCRAMSHA1;
+  MD5 = RNXMPP.DigestMD5;
 
-    constructor(){
-        this.isConnected = false;
-        this.isLogged = false;
-        this.listeners = [
-            NativeAppEventEmitter.addListener(map.connect, this.onConnected.bind(this)),
-            NativeAppEventEmitter.addListener(map.disconnect, this.onDisconnected.bind(this)),
-            NativeAppEventEmitter.addListener(map.error, this.onError.bind(this)),
-            NativeAppEventEmitter.addListener(map.loginError, this.onLoginError.bind(this)),
-            NativeAppEventEmitter.addListener(map.login, this.onLogin.bind(this)),
-        ];
+  constructor() {
+    this.isConnected = false;
+    this.isLogged = false;
+    this.listeners = [
+      emitter.addListener(map.connect, this.onConnected.bind(this)),
+      emitter.addListener(map.disconnect, this.onDisconnected.bind(this)),
+      emitter.addListener(map.error, this.onError.bind(this)),
+      emitter.addListener(map.loginError, this.onLoginError.bind(this)),
+      emitter.addListener(map.login, this.onLogin.bind(this)),
+    ];
+  }
+
+  onConnected() {
+    LOG('Connected');
+    this.isConnected = true;
+  }
+
+  onLogin() {
+    LOG('Login');
+    this.isLogged = true;
+  }
+
+  onDisconnected(error) {
+    LOG('Disconnected, error: ' + error);
+    this.isConnected = false;
+    this.isLogged = false;
+  }
+
+  onError(text) {
+    LOG('Error: ' + text);
+  }
+
+  onLoginError(text) {
+    this.isLogged = false;
+    LOG('LoginError: ' + text);
+  }
+
+  on(type, callback) {
+    if (map[type]) {
+      const listener = emitter.addListener(map[type], callback);
+      this.listeners.push(listener);
+      return listener;
+    } else {
+      throw 'No registered type: ' + type;
     }
+  }
 
-    onConnected(){
-        LOG("Connected");
-        this.isConnected = true;
-    }
-
-    onLogin(){
-        LOG("Login");
-        this.isLogged = true;
-    }
-
-    onDisconnected(error){
-        LOG("Disconnected, error: "+error);
-        this.isConnected = false;
-        this.isLogged = false;
-    }
-
-    onError(text){
-        LOG("Error: "+text);
-    }
-
-    onLoginError(text){
-        this.isLogged = false;
-        LOG("LoginError: "+text);
-    }
-
-    on(type, callback){
-        if (map[type]){
-            const listener = NativeAppEventEmitter.addListener(map[type], callback);
-            this.listeners.push(listener);
-            return listener;
-        } else {
-            throw "No registered type: " + type;
+  removeListener(type) {
+    if (map[type]) {
+      this.listeners = this.listeners.filter(listener => {
+        if (listener.eventType === map[type]) {
+          listener.remove();
+          LOG(`Event listener of type "${type}" removed`);
+          return false;
         }
+        return true;
+      });
     }
+  }
 
-    removeListener(type) {
-        if (map[type]) {
-            for (var i = 0; i < this.listeners.length; i++) {
-                var listener = this.listeners[i];
-                if (listener.eventType === map[type]) {
-                    listener.remove();
-                    var index = this.listeners.indexOf(listener);
-                    if (index > -1) {
-                        this.listeners.splice(index, 1);
-                    }
-                    LOG(`Event listener of type "${type}" removed`);
-                }
-            }
-        }
-    }
+  removeListeners() {
+    this.listeners.forEach(listener => listener.remove());
+    this.listeners = [
+      emitter.addListener(map.connect, this.onConnected.bind(this)),
+      emitter.addListener(map.disconnect, this.onDisconnected.bind(this)),
+      emitter.addListener(map.error, this.onError.bind(this)),
+      emitter.addListener(map.loginError, this.onLoginError.bind(this)),
+      emitter.addListener(map.login, this.onLogin.bind(this)),
+    ];
+    LOG('All event listeners removed');
+  }
 
-    removeListeners() {
-        for (var i = 0; i < this.listeners.length; i++) {
-            this.listeners[i].remove();
-        }
+  trustHosts(hosts) {
+    RNXMPP.trustHosts(hosts);
+  }
 
-        this.listeners = [
-            NativeAppEventEmitter.addListener(map.connect, this.onConnected.bind(this)),
-            NativeAppEventEmitter.addListener(map.disconnect, this.onDisconnected.bind(this)),
-            NativeAppEventEmitter.addListener(map.error, this.onError.bind(this)),
-            NativeAppEventEmitter.addListener(map.loginError, this.onLoginError.bind(this)),
-            NativeAppEventEmitter.addListener(map.login, this.onLogin.bind(this)),
-        ];
-        
-        LOG('All event listeners removed');
+  connect(username, password, auth = RNXMPP.SCRAMSHA1, hostname = null, port = 5222) {
+    if (!hostname) {
+      hostname = (username + '@/').split('@')[1].split('/')[0];
     }
+    RNXMPP.connect(username, password, auth, hostname, port);
+  }
 
-    trustHosts(hosts){
-        React.NativeModules.RNXMPP.trustHosts(hosts);
-    }
+  message(text, user, thread = null) {
+    LOG(`Message: "${text}" being sent to user: ${user}`);
+    RNXMPP.message(text, user, thread);
+  }
 
-    connect(username, password, auth = RNXMPP.SCRAMSHA1, hostname = null, port = 5222){
-        if (!hostname){
-            hostname = (username+'@/').split('@')[1].split('/')[0];
-        }
-        React.NativeModules.RNXMPP.connect(username, password, auth, hostname, port);
-    }
+  sendStanza(stanza) {
+    RNXMPP.sendStanza(stanza);
+  }
 
-    message(text, user, thread = null){
-        LOG(`Message: "${text}" being sent to user: ${user}`);
-        React.NativeModules.RNXMPP.message(text, user, thread);
-    }
+  fetchRoster() {
+    RNXMPP.fetchRoster();
+  }
 
-    sendStanza(stanza){
-        RNXMPP.sendStanza(stanza);
-    }
+  presence(to, type) {
+    RNXMPP.presence(to, type);
+  }
 
-    fetchRoster(){
-        RNXMPP.fetchRoster();
-    }
+  removeFromRoster(to) {
+    RNXMPP.removeRoster(to);
+  }
 
-    presence(to, type){
-        React.NativeModules.RNXMPP.presence(to, type);
+  disconnect() {
+    if (this.isConnected) {
+      RNXMPP.disconnect();
     }
+  }
 
-    removeFromRoster(to){
-        React.NativeModules.RNXMPP.removeRoster(to);
+  disconnectAfterSending() {
+    if (this.isConnected) {
+      RNXMPP.disconnectAfterSending();
     }
-
-    disconnect(){
-        if (this.isConnected){
-            React.NativeModules.RNXMPP.disconnect();
-        }
-    }
-    disconnectAfterSending(){
-      if (this.isConnected){
-        React.NativeModules.RNXMPP.disconnectAfterSending();
-      }
-    }
+  }
 }
 
 module.exports = new XMPP();
